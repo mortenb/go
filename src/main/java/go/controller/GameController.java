d942074b2491584589b1922d693a17234d630028
Morten Byhring <mbyhring@gmail.com>
2015-04-05 15:52:46 +0200

    added management beans content added to test classes etc

--- src/main/java/go/controller/GameController.java
+++ src/main/java/go/controller/GameController.java
@@ -1,7 +1,16 @@
 package go.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by moby on 05/04/15.
  */
+@RestController
 public class GameController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello";
+    }
 }
--- src/test/java/go/integration/GameControllerIntegrationTest.java
+++ src/test/java/go/integration/GameControllerIntegrationTest.java
@@ -1,19 +1,28 @@
 package go.integration;
 
+import go.GoApplication;
 import go.controller.GameController;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URL;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -22,21 +31,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = MockServletContext.class)
+@SpringApplicationConfiguration(classes = GoApplication.class)
 @WebAppConfiguration
+@IntegrationTest({"server.port=0"})
 public class GameControllerIntegrationTest {
 
-    private MockMvc mvc;
+    @Value("${local.server.port}")
+    private int port;
+
+    private URL base;
+
+    private RestTemplate restTemplate;
+
 
     @Before
     public void setup() throws Exception{
-        mvc = MockMvcBuilders.standaloneSetup(new GameController()).build();
+        this.base = new URL("http://localhost:" + port + "/");
+        restTemplate = new TestRestTemplate();
     }
 
     @Test
     public void testHello() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Hello")));
+        ResponseEntity<String> response = restTemplate.getForEntity(base.toString(), String.class);
+        assertThat(response.getBody(), equalTo("Hello"));
     }
 }
