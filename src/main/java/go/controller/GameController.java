fa49ed65831f4b4a9a9a12fc346e27036e71c08c
Morten Byhring <mbyhring@gmail.com>
2015-04-05 15:46:10 +0200

    Init: added test controller and tests

---
+++ src/main/java/go/controller/GameController.java
@@ -0,0 +1,7 @@
+package go.controller;
+
+/**
+ * Created by moby on 05/04/15.
+ */
+public class GameController {
+}
---
+++ src/test/java/go/controller/GameControllerTest.java
@@ -0,0 +1,10 @@
+package go.controller;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by moby on 05/04/15.
+ */
+public class GameControllerTest {
+
+}
